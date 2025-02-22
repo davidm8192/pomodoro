6f779a41b59fae205972e70545d197ff36450e18
khushishahs <131717382+khushishahs@users.noreply.github.com>
2025-02-21 23:48:19 -0500

    Added constants for home page

---
+++ app/utils/color-constants.ts
@@ -0,0 +1,10 @@
+export const THEME_STANDARD = {
+    COLOR1: "#C7E9FF",
+    COLOR2: "#FFF7B1",
+    COLOR3: "#B2DA9C",
+    COLOR4: "#D9D39B",
+    TITLE_COLOR: "#000000",
+    LABEL_COLOR1: "#5D4237",
+    LABEL_COLOR2: "#47573E"
+};
+
---
+++ app/utils/constants.ts
@@ -0,0 +1,40 @@
+// home page titles
+export const HOME_TITLE = "POMODORO";
+export const HOME_SUBTITLE = "TIME!";
+
+// home page buttons
+export const SESSION_BUTTON = {
+    LABEL: "sessions",
+    OPTION1: "1",
+    OPTION2: "2",
+    OPTION3: "3",
+    OPTION4: "4",
+    OPTION5: "5",
+    OPTION6: "∞"
+};
+
+export const STUDY_BUTTON = {
+    LABEL: "study",
+    OPTION1: "15",
+    OPTION2: "25",
+    OPTION3: "30",
+    OPTION4: "45",
+    OPTION5: "60"
+};
+
+export const BREAK_BUTTON = {
+    LABEL: "break",
+    OPTION1: "5",
+    OPTION2: "10",
+    OPTION3: "15",
+    OPTION4: "20",
+    OPTION5: "25"
+};
+
+export const TIME_UNIT = "minutes";
+
+export const START_BUTTON = "start";
+
+export const CANCEL_BUTTON = "cancel";
+
+export const SETTINGS_COMMENT = "customize time, sounds, and theme!";
